a18d24c24c5a913cdd53b37e3f18f3ff245282fb
jaychacko <chackojayui@gmail.com>
2017-08-11 00:22:32 -0500

    text

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,14 +1,21 @@
 import React , {Component} from 'react';
-import {View} from 'react-native';
+import {TextInput} from 'react-native';
 import {Button, Card, CardSection} from './common/'
 
 
 class LoginForm extends Component {
+    state = {text:''};
     render(){
         return (
            <Card>
-               <CardSection/>
-               <CardSection/>
+               <CardSection>
+               <TextInput 
+               value={this.state.text}
+               onChange={()=>{
+                 this.state({text})
+                   }} 
+                   style={{height:20,width:100}}/>
+               </CardSection>
 
                <CardSection>
                    <Button>
